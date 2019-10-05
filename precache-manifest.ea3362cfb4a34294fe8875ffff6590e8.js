45f4a1bce2cf2b79a49dba8605c79d2c3a4aa728
Henry <ibooshyou@gmail.com>
2019-10-05 13:43:58 +0100

    Updates

---
+++ precache-manifest.ea3362cfb4a34294fe8875ffff6590e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0877904cc171286eb6f5fcc2c4f7a499",
+    "url": "/monsters-rolo/index.html"
+  },
+  {
+    "revision": "e13028ed30dd88246d2d",
+    "url": "/monsters-rolo/static/css/main.14f6de41.chunk.css"
+  },
+  {
+    "revision": "db5eea2cd8591c1329b8",
+    "url": "/monsters-rolo/static/js/2.81e14308.chunk.js"
+  },
+  {
+    "revision": "e13028ed30dd88246d2d",
+    "url": "/monsters-rolo/static/js/main.fe1afd8d.chunk.js"
+  },
+  {
+    "revision": "dfbd95332f3cf8d3aee4",
+    "url": "/monsters-rolo/static/js/runtime-main.ecccfae3.js"
+  }
+]);
